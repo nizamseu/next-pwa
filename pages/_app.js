743b801393a0b1ff615c09e5b7eca2ddb5f06351
Nizam Uddin Babu <43912849+nizamseu@users.noreply.github.com>
2022-04-21 19:38:41 +0600

    something error

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,11 @@
 import Layout from "../Components/layout/Layout";
 
 import "../styles/globals.scss";
-
+import TestManu from "../Components/ManuBar/TestManu";
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
+      <TestManu />
       <Component {...pageProps} />
     </Layout>
   );
