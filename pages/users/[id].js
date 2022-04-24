b112300f18deef4b5b7e04fe7de59f4a5d40084d
Nizam Uddin Babu <43912849+nizamseu@users.noreply.github.com>
2022-04-25 01:33:58 +0600

    user design isdone

--- pages/users/[id].js
+++ pages/users/[id].js
@@ -1,6 +1,7 @@
 import { useRouter } from "next/router";
 import React from "react";
-
+import UserIcon from "../../Images/personalLoan/man.png";
+import Image from "next/image";
 export const getServerSideProps = async (ctx) => {
   const { params } = ctx;
 
@@ -21,19 +22,100 @@ const singleUser = ({ data }) => {
   const router = useRouter();
   const id = router.query.id;
   console.log(data);
-
-  return (
-    <div className="flex flex-col ">
-      <h1 className="text-5xl text-center mb-5">Single User {id}</h1>
-      <div className="justify-center align-center mx-auto">
-        <h1>{data.name}</h1>
+  {
+    /* <h1>{data.name}</h1>
         <h1>{data.email}</h1>
         <h1>{data.username}</h1>
         <h1>{data.phone}</h1>
         <h1>{data.website}</h1>
-        <h1>{data.name}</h1>
+        <h1>{data.name}</h1> */
+  }
+  return (
+    <div className="flex flex-col mt-8">
+      {/* <h1 className="text-5xl text-center mb-5">Single User {id}</h1> */}
+      <div
+        style={{ boxShadow: "rgba(0, 0, 0, 0.35) 0px 5px 15px" }}
+        className="grid lg:grid-cols-2 md:grid-cols justify-center align-center mx-auto shadow-xl rounded p-6"
+      >
+        <div className="m-4 p-4">
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+            }}
+          >
+            <Image
+              width="200"
+              height="200"
+              className="p-4 border-2 border-emerald-300 rounded-full "
+              src={UserIcon}
+              alt="Rounded avatar"
+            />
+          </div>
+          <div className="w-full bg-gray-200 rounded-full h-2.5 mt-8">
+            <div
+              className="bg-blue-600 h-2.5 rounded-full my-2"
+              style={{ width: "85%" }}
+            ></div>
+          </div>
+          <div className="flex justify-between items-center my-4">
+            <div className="mx-4">
+              <h1 className="my-2 text-neutral-400 text-3xl">Your Rating</h1>
+              <h1 className="text-center font-bold text-5xl">51%</h1>
+            </div>
+            <div className="mx-4">
+              <h1 className="my-2 text-neutral-400 text-3xl">Your Activity</h1>
+              <h1 className="text-center font-bold text-5xl">81%</h1>
+            </div>
+          </div>
+        </div>
+        {/* information section  */}
+        <div>
+          {/* nae and address  */}
+          <div className="m-4 p-4">
+            <h1 className="text-5xl font-bold text-cyan-700">{data.name}</h1>
+            <h1 className="text-xl ">{`${data.address.street} ,${data.address.city}`}</h1>
+            <hr className="my-4" />
+          </div>
+
+          {/* personal info  */}
+          <div>
+            <div className="grid grid-cols-2 my-1">
+              <h1 className="text-xl text-left">Email</h1>
+              <h1 className="text-xl text-left">{data.email}</h1>
+            </div>
+            <div className="grid grid-cols-2 my-1">
+              <h1 className="text-xl text-left">User Name</h1>
+              <h1 className="text-xl text-left">{data.username}</h1>
+            </div>
+            <div className="grid grid-cols-2 my-1">
+              <h1 className="text-xl text-left">Web Site</h1>
+              <h1 className="text-xl text-left">{data.website}</h1>
+            </div>
+            <div className="grid grid-cols-2 my-1">
+              <h1 className="text-xl text-left">Address</h1>
+              <h1 className="text-xl text-left">{`${data.address.street} ,${data.address.city}`}</h1>
+            </div>
+            <div className="grid grid-cols-2 my-1">
+              <h1 className="text-xl text-left">Phone</h1>
+              <h1 className="text-xl text-left">{data.phone}</h1>
+            </div>
+            <div className="grid grid-cols-2 my-1">
+              <h1 className="text-xl text-left">Company</h1>
+              <div>
+                <h1 className="text-xl text-left">{data.company.name}</h1>
+                <button
+                  onClick={() => router.back()}
+                  type="button"
+                  className="w-24 my-12 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+                >
+                  Back
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-      <button onClick={() => router.back()}>Back</button>
     </div>
   );
 };
--- pages/users/index.js
+++ pages/users/index.js
@@ -1,5 +1,9 @@
 import React from "react";
 import Link from "next/link";
+import Image from "next/image";
+import UserIcon from "../../Images/personalLoan/man.png";
+import Location from "../../Images/Others/location.gif";
+
 export const getServerSideProps = async () => {
   const result = await fetch("https://jsonplaceholder.typicode.com/users");
   const data = await result.json();
@@ -11,22 +15,66 @@ export const getServerSideProps = async () => {
 };
 
 const index = ({ users }) => {
-  console.log(users);
   return (
-    <div>
-      <h1>All User List</h1>
-      {users &&
-        users.map((item) => {
-          return (
-            <>
-              <div className="border-solid border-2 m-2 p-3">
-                <Link href={`/users/${item.id}`} passHref>
-                  <h1>{item.name}</h1>
-                </Link>
-              </div>
-            </>
-          );
-        })}
+    <div className="mt-2">
+      <h1 className="text-4xl text-center p-8">All User List</h1>
+      <div className="px-24   grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 g gap-6">
+        {users &&
+          users.map((item) => {
+            return (
+              <>
+                <div
+                  style={{ height: "450px" }}
+                  className="shadow-lg border-2 rounded-md"
+                >
+                  <Link href={`/users/${item.id}`} passHref>
+                    <a>
+                      <div className=" m-2 p-3 ">
+                        {/* image and name  */}
+                        <div className=" flex flex-col align-middle items-center justify-center">
+                          <div className="p-4">
+                            <div className="flex justify-center items-center mb-8 ">
+                              <Image
+                                height="10px"
+                                width="10px"
+                                src={Location}
+                                alt=""
+                              />
+                              <h1 className="ml-2 text-center">
+                                {item.address.city}
+                              </h1>
+                            </div>
+                            <Image
+                              width="130px"
+                              height="130px"
+                              className="p-4 border-2 border-emerald-300 rounded-full  "
+                              src={UserIcon}
+                              alt="Rounded avatar"
+                            />
+                          </div>
+                          <h1
+                            className="font-bold text-3xl pb-4 text-center"
+                            style={{ color: "#021533" }}
+                          >
+                            {item.name}
+                          </h1>
+                          <hr className="divide-y " />
+                          <h1 className="text-xl text-center">
+                            {item?.company?.catchPhrase}
+                          </h1>
+                          <h1 className="text-sm text-center my-2">
+                            {" "}
+                            {item?.company?.bs}
+                          </h1>
+                        </div>
+                      </div>
+                    </a>
+                  </Link>
+                </div>
+              </>
+            );
+          })}
+      </div>
     </div>
   );
 };
